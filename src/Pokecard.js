b44ec9e21ea2659df570ef4a32cde1f742e02ea5
JaeWon Jin <j1jin286@gmail.com>
2023-10-30 16:24:24 -0700

    Fixed todos

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,20 +1,18 @@
 import "./Pokecard.css";
-//TODO:should have docstring
+const POKE_API = "https://raw.githubusercontent.com/PokeAPI";
 
 function Pokecard({ id, name, type, exp }) {
 
-  const pokeImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  const pokeImage = `${POKE_API}/sprites/master/sprites/pokemon/${id}.png`;
 
   return (
-    <div className="Pokecard-card">
-      <p className="Pokecard-card-name">{name}</p>
+    <div className="Pokecard">
+      <p className="Pokecard-name">{name}</p>
       <img src={pokeImage} alt={`This is a card of ${name}`} />
       <p>Type: {type}</p>
       <p>EXP: {exp}</p>
     </div>
   );
 }
-//TODO:make pokeImage global variable
-//TODO:rename className without -
 
 export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,18 @@
 import Pokecard from "./Pokecard";
 import "./Pokedex.css";
 
-function Pokedex({ pokemon }) {
+const POKEMON = [
+  { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+  { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+  { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+  { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
+  { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
+  { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
+  { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+  { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+];
+
+function Pokedex({ pokemon = POKEMON }) {
   return (
     <div className="Pokedex">
       <h1>Pokedex</h1>
